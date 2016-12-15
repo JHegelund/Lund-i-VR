0f05b4356cc82eb7f0457efde92dee0ad9709793
gustavryrlen <gustavryrlen@gustavs-MacBook-Pro-2.local>
2016-12-15 16:13:15 +0100

    initial

---
+++ estate/Assets/moviescript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moviescript : MonoBehaviour {
+
+	/*public MovieTexture movTexture;*/
+
+
+
+
+		void Start() {
+		Handheld.PlayFullScreenMovie("Materials/ILL-BE-AROUND-THE-SPINNERS.ogv", Color.black, FullScreenMovieControlMode.CancelOnInput);
+
+		/*GetComponent<Renderer>().material.mainTexture = movTexture;
+			movTexture.Play();*/
+		}
+
+	}
---
+++ estate/Assets/scripts/ResizeMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResizeMenu : MonoBehaviour {
+	public int i = 0;
+	// Use this for initialization
+	public void resize(){
+		if (i == 0) {
+			i = 1;
+			transform.localScale += new Vector3 (-0.6F, 0, 0F);
+		} else {
+			i = 0;
+			transform.localScale += new Vector3 (0.6F, 0, 0F);
+		}
+	}
+}
